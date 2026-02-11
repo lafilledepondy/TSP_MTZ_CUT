8f6a5db8ca0bfc0bd1c239b943f06f4ee3caa7b0
gaia <gayathiri.ravendirane@gmail.com>
2026-02-11 08:30:44 +0100

    code without the latex related codes

--- src/ATSP_CUT.cpp
+++ src/ATSP_CUT.cpp
@@ -6,30 +6,14 @@ ATSP_CUT::ATSP_CUT(ATSPDataC data) : data(data) {}
 
 void ATSP_CUT::solve()
 {
-    SolveOptions options;
-    options.timeLimitSec = 600.0;
-    options.threads = 1;
-    options.logFile = "atsp_cut.log";
-    solveWithOptions(options);
-}
-
-SolveMetrics ATSP_CUT::solveWithOptions(const SolveOptions &options)
-{
-    SolveMetrics metrics;
     try
     {
         env = std::make_unique<GRBEnv>(true);
-        if (!options.logFile.empty())
-        {
-            env->set("LogFile", options.logFile);
-        }
+        env->set("LogFile", "atsp_cut.log");
         env->start();
 
         model = std::make_unique<GRBModel>(*env);
         GRBModel &modelRef = *model;
-
-        char xType = options.relax ? GRB_CONTINUOUS : GRB_BINARY;
-        char uType = options.relax ? GRB_CONTINUOUS : GRB_INTEGER;
 
         // Variables
         x.assign(data.size, vector<GRBVar>(data.size));
@@ -41,17 +25,17 @@ SolveMetrics ATSP_CUT::solveWithOptions(const SolveOptions &options)
             // 1 <= u_i <= n-1 for i in N\{0}
             if (i != 0)
             {
-                u[i] = modelRef.addVar(1.0, data.size - 1, 0.0, uType, "u(" + to_string(i) + ")");
+                u[i] = modelRef.addVar(1.0, data.size - 1, 0.0, GRB_INTEGER, "u(" + to_string(i) + ")");
             }
             else
             {
-                u[i] = modelRef.addVar(0.0, 0.0, 0.0, uType, "u(" + to_string(i) + ")");
+                u[i] = modelRef.addVar(0.0, 0.0, 0.0, GRB_INTEGER, "u(" + to_string(i) + ")");
             }
             for (int j = 0; j < data.size; ++j)
             {
                 if (i != j)
                 {
-                    x[i][j] = modelRef.addVar(0.0, 1.0, data.distances[i][j], xType, "x(" + to_string(i) + "," + to_string(j) + ")");
+                    x[i][j] = modelRef.addVar(0.0, 1.0, data.distances[i][j], GRB_BINARY, "x(" + to_string(i) + "," + to_string(j) + ")");
                 }
             }
         }
@@ -86,36 +70,18 @@ SolveMetrics ATSP_CUT::solveWithOptions(const SolveOptions &options)
             modelRef.addConstr(in == 1);
         }
 
-        if (!options.relax)
-        {
-            modelRef.set(GRB_IntParam_LazyConstraints, 1);
-        }
+        modelRef.set(GRB_IntParam_LazyConstraints, 1);
 
         std::unique_ptr<ATSP_CUT_Callback> cb;
-        if (!options.relax)
-        {
-            cb = std::unique_ptr<ATSP_CUT_Callback>(new ATSP_CUT_Callback(data.size, x));
-            modelRef.setCallback(cb.get());
-        }
+        cb = std::unique_ptr<ATSP_CUT_Callback>(new ATSP_CUT_Callback(data.size, x));
+        modelRef.setCallback(cb.get());
 
-        modelRef.set(GRB_DoubleParam_TimeLimit, options.timeLimitSec);
-        modelRef.set(GRB_IntParam_Threads, options.threads);
-        if (options.writeModel)
-        {
-            modelRef.write("model.lp");
-        }
+        modelRef.set(GRB_DoubleParam_TimeLimit, 180.0);
+        modelRef.set(GRB_IntParam_Threads, 1);
+
+        modelRef.write("model.lp");
         modelRef.optimize();
         setterStatus(modelRef.get(GRB_IntAttr_Status));
-
-        metrics.status = modelRef.get(GRB_IntAttr_Status);
-        metrics.solCount = modelRef.get(GRB_IntAttr_SolCount);
-        metrics.runtime = modelRef.get(GRB_DoubleAttr_Runtime);
-        metrics.nodeCount = modelRef.get(GRB_DoubleAttr_NodeCount);
-        metrics.hasSolution = (metrics.solCount > 0);
-        if (metrics.hasSolution)
-        {
-            metrics.objVal = modelRef.get(GRB_DoubleAttr_ObjVal);
-        }
     }
     catch (GRBException e)
     {
@@ -126,8 +92,6 @@ SolveMetrics ATSP_CUT::solveWithOptions(const SolveOptions &options)
     {
         cout << "Exception during optimization" << endl;
     }
-
-    return metrics;
 }
 
 void ATSP_CUT::findSubtour_S(const int sol, vector<int> &S)
